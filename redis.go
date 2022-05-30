87e88ccc79c835084e92afc75a2c3215125cf0a2
jishulangcom <57217780+jishulangcom@users.noreply.github.com>
2022-05-30 22:52:37 +0800

    Add files via upload

--- redis.go
+++ redis.go
@@ -2,14 +2,14 @@ package config
 
 type RedisCnfDto struct {
 	Host string `yaml:"host" toml:"host" json:"host" bson:"host"` // 地址
-	Port string `yaml:"port" toml:"port" json:"port" bson:"port"` // 端口
+	Port int `yaml:"port" toml:"port" json:"port" bson:"port"` // 端口
 	Pwd  string `yaml:"pwd" toml:"pwd" json:"pwd" bson:"pwd"`     // 密码
 	DB   int    `yaml:"db" toml:"db" json:"db" bson:"db"`         // 库
 }
 
 var RedisCnf RedisCnfDto = RedisCnfDto{
 	Host: "127.0.0.1",
-	Port: "6379",
+	Port: 6379,
 	Pwd:  "",
 	DB:   0,
 }
