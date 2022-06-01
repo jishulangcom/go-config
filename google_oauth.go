5f9c946dfd2d99e3a685cc57c65444571dcd5509
jishulangcom <57217780+jishulangcom@users.noreply.github.com>
2022-06-01 09:15:06 +0800

    Add files via upload

---
+++ google_oauth.go
@@ -0,0 +1,8 @@
+package config
+
+type GoogleOAuthCnfDto struct {
+	ClientID     string `yaml:"client_id" toml:"client_id" json:"client_id" bson:"client_id"`                 // 客户端ID
+	ClientSecret string `yaml:"client_secret" toml:"client_secret" json:"client_secret" bson:"client_secret"` // 客户端密钥
+	RedirectURL  string `yaml:"redirect_url" toml:"redirect_url" json:"redirect_url" bson:"redirect_url"`     // 回调地址
+	Debug        bool   `yaml:"debug" toml:"debug" json:"debug" bson:"debug"`                                 // 调试
+}
